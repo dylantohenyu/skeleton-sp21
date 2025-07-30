2511b0dffc04f46b1cce067a27a27997e1cd34d9
Dylan Toh <dylantoh@berkeley.edu>
2025-07-30 16:20:51 +0800

    Done with collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,16 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Dylan Toh
  */
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n  % 2 == 0) {
+            return n/2; /* if n is even, divide it by 2 */
+        } else if (n == 1) {
+            return n; /* if n is 1, return n */
         } else {
-            return n * 2;
+            return 3 * n + 1; /* if n is odd, multiply by 3 and add 1 */
         }
     }
 
